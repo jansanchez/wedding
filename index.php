15ebe5d2c92c6c51d1d02076600b2b6ac7a2e0d3
Frontend Labs <frontendlabsperu@gmail.com>
2018-05-06 17:50:41 -0400

    design: changes

--- index.php
+++ index.php
@@ -9,23 +9,23 @@
   <body>
 
 		<div id="app" class="box is-mobile">
-			<div id="mainEnvelope" class="container envelope">
-				<div id="envelope" class="box has-text-centered">
+			<div id="mainEnvelope" class="container envelope is-mobile">
+				<div id="envelope" class="box has-text-centered is-mobile">
 					<h1>Para: <?php echo $_GET['name']; ?></h1>
 				</div>
 			</div>
 
-			<div class="container">
-				<div id="invitation" class="box has-text-centered">
-					<div class="header has-text-centered">
+			<div class="container is-mobile">
+				<div id="invitation" class="box has-text-centered is-mobile">
+					<div class="header has-text-centered is-mobile">
 						<h1>Angela<br>
 							&<br>
 							Billy</h1>
 					</div>
-					<div class="body has-text-centered">
+					<div class="body has-text-centered is-mobile">
 						<h2>Te invitamos a celebrar<br>nuestro matrimonio</h2>
 						<h3>0&nbsp;2&nbsp;&nbsp;&nbsp;D&nbsp;E&nbsp;&nbsp;&nbsp;J&nbsp;U&nbsp;N&nbsp;I&nbsp;O</h3>
-						<h2>Mz. C Lt. 9<br>Virgen de Fátima - SMP <a href="https://bit.ly/2KFCsgC" target="_blank" title="ver mapa">(mapa)</a></h2>
+						<h2>Mz. C Lt. 9<br>Virgen de Fátima - SMP <a href="https://bit.ly/2rprMtp" target="_blank" title="ver mapa">(mapa)</a></h2>
 						<h2 class="schedule">|&nbsp;&nbsp;&nbsp;6:30&nbsp;pm&nbsp;&nbsp;&nbsp;|</h2>
 					</div>
 				</div>					
